89c8eb6b2a4aee4865289ccf1ba44ed8c38bb79b
tmattick <tmattick@smail.uni-koeln.de>
2022-01-20 09:20:26 +0100

    Equals-Operator
    Added the __eq__ method for PDF class by comparing r and g

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+from typing import List
+import numpy as np
+
+
 class PDF:
-    def __init__(self, r, g):
-        self.r = r
-        self.g = g
+    def __init__(self, r: List, g: List):
+        if isinstance(r, np.array):
+            self.r = r
+        else:
+            self.r = np.array(r)
+        if isinstance(r, np.array):
+            self.g = g
+        else:
+            self.g = np.array(g)
+    
+
+    def __eq__(self, __o: object) -> bool:
+        return self.r == __o.r and self.g == __o.g
+    
