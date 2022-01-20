a8e27163d14c7dabf4e2b1b00a980b1b6abf8420
tmattick <tmattick@smail.uni-koeln.de>
2022-01-20 09:29:53 +0100

    Added calculation of differential PDFs
    Added a static method differential_pdf to PDF class, that calculates
    the differential PDFs, provided they share a r axis. Raises an Exception
    otherwise

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from typing import List
 import numpy as np
 
 
+class XAxisException(Exception):
+    def __init__(self, x1, x2, message="The x axes provided do not fit."):
+        self.x1 = x1
+        self.x2 = x2
+        self.message = message
+        super().__init__(self.message)
+
+
 class PDF:
     def __init__(self, r: List, g: List):
         if isinstance(r, np.array):
@@ -16,4 +24,13 @@ class PDF:
 
     def __eq__(self, __o: object) -> bool:
         return self.r == __o.r and self.g == __o.g
+
+    
+    @staticmethod
+    def differential_pdf(pdf1, pdf2):
+        if pdf1.r == pdf2.r:
+            g = pdf1.g - pdf2.g
+            return PDF(pdf1.r, g)
+        else:
+            raise XAxisException(pdf1.r, pdf2.r)
     
