196486ad1ed6662beb0716500858a86f69de8f4e
tmattick <tmattick@smail.uni-koeln.de>
2022-01-21 16:13:46 +0100

    Added Plotting
    Added a matplotlib canvas that refreshes whenever a new PDF is added

--- gui.py
+++ gui.py
@@ -1,15 +1,16 @@
 import PySimpleGUI as sg
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
 import matplotlib
 from pdf import PDF
 
 sg.theme("SystemDefault")
 pdfs = []
-pdf_names = []
 
 # Matplotlib
 fig = matplotlib.figure.Figure(figsize=(5,4), dpi=100)
 matplotlib.use("TkAgg")
+sub = fig.add_subplot(111)
 
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
@@ -17,17 +18,23 @@ def draw_figure(canvas, figure):
     figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
     return figure_canvas_agg
 
+
+def delete_fig(fig_agg):
+    fig_agg.get_tk_widget().forget()
+    plt.close("all")
+
+    
 # PySimpleGUI
 left_layout = [
     [sg.Text("File:"), sg.In(size=(25,1), enable_events=True, key="-FILE_IN-"), sg.FileBrowse()],
-    [sg.Listbox(values=pdf_names, enable_events=True, size=(40,20), key="-PDF_LIST-")],
+    [sg.Listbox(values=pdfs, enable_events=True, size=(40,20), key="-PDF_LIST-")],
     [sg.Button("Import", key="-IMPORT_BUTTON-")]
     ]
 right_layout = [[sg.Canvas(size=(40,40), key="-CANVAS-")]]
 layout = [[sg.Column(left_layout), sg.VSeperator(), sg.Column(right_layout)]]
 
 window = sg.Window("PDFview", layout=layout, finalize=True)
-fig_canvas_agg = draw_figure(window["-CANVAS-"].TKCanvas, fig)
+fig_agg = draw_figure(window["-CANVAS-"].TKCanvas, fig)
 
 
 if __name__ == "__main__":
@@ -43,7 +50,10 @@ if __name__ == "__main__":
         elif event == "-IMPORT_BUTTON-":
             path = window["-FILE_IN-"].get()
             pdfs.append(PDF.read_gr_file(path))
-            pdf_names.append(pdfs[-1].name)
-            window["-PDF_LIST-"].update(pdf_names)
+            window["-PDF_LIST-"].update(pdfs)
+            delete_fig(fig_agg)
+            sub.plot(pdfs[-1].r, pdfs[-1].g)
+            
+            fig_agg = draw_figure(window["-CANVAS-"].TKCanvas, fig)
 
     window.close()
