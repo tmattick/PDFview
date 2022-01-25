97c2ef5ed5c942da0345afb2cab4f0044bb8f40c
tmattick <tmattick@smail.uni-koeln.de>
2022-01-25 12:48:44 +0100

    Comments/Type hints

--- gui.py
+++ gui.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
             break
         elif event == "-IMPORT_BUTTON-":
             # import new PDF from file
-            path = window["-FILE_IN-"].get()
+            path: str = window["-FILE_IN-"].get()
             pdfs.append(PDF.read_gr_file(path))
             window["-PDF_LIST-"].update(pdfs)
             delete_fig(fig_agg)
@@ -79,10 +79,10 @@ if __name__ == "__main__":
                     diff_run = False
                     break
                 elif diff_event == "-DIFF_BUTTON-":
-                    minuend = diff_values["-PDF_MINUENDS-"][0]
-                    subtrahend = diff_values["-PDF_SUBTRAHENDS-"][0]
+                    minuend: PDF = diff_values["-PDF_MINUENDS-"][0]
+                    subtrahend: PDF = diff_values["-PDF_SUBTRAHENDS-"][0]
                     try:
-                        diff_pdf = PDF.differential_pdf(minuend, subtrahend)
+                        diff_pdf: PDF = PDF.differential_pdf(minuend, subtrahend)
                         pdfs.append(diff_pdf)
                         window["-PDF_LIST-"].update(pdfs)
                         delete_fig(fig_agg)
@@ -95,8 +95,9 @@ if __name__ == "__main__":
 
             diff_window.close()
         elif event == "-SCALE_BUTTON-":
+            # scale a PDF to a multiple of itself
             try:
-                pdf = values["-PDF_LIST-"][0]
+                pdf: PDF = values["-PDF_LIST-"][0]
                 pdf.scale(float(values["-SCALE_IN-"]))
                 delete_fig(fig_agg)
                 fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
@@ -111,11 +112,12 @@ if __name__ == "__main__":
             except IndexError as e:
                 sg.popup_error("Choose a PDF to scale.")
         elif event == "-FIT_BUTTON-":
+            # fit the chosen PDF to another one via scaling
             try:
                 fit_layout = [[sg.Text("Choose a PDF to scale to.")],
                               [sg.Listbox(values=pdfs, key="-FIT_TO_PDFS-")],
                               [sg.Button("OK", key="-FIT_BUTTON-")]]
-                pdf = values["-PDF_LIST-"][0]
+                pdf: PDF = values["-PDF_LIST-"][0]
 
                 fit_window = sg.Window("Scale to...", layout=fit_layout)
                 fit_run = True
@@ -126,7 +128,7 @@ if __name__ == "__main__":
                         fit_run = False
                         break
                     elif fit_event == "-FIT_BUTTON-":
-                        fit_pdf = fit_values["-FIT_TO_PDFS-"][0]
+                        fit_pdf: PDF = fit_values["-FIT_TO_PDFS-"][0]
                         pdf.scale_to_pdf(fit_pdf)
                         delete_fig(fig_agg)
                         fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
@@ -148,8 +150,9 @@ if __name__ == "__main__":
             except XAxisException as e:
                 sg.popup_error("The provided PDFs don't share a r axis. Fit could not be calculated.")
         elif event == "-SAVE_PATH-":
+            # save the chosen PDF
             try:
-                pdf = values["-PDF_LIST-"][0]
+                pdf: PDF = values["-PDF_LIST-"][0]
                 pdf.save_gr_file(values["-SAVE_PATH-"])
             except IndexError as e:
                 sg.popup_error("Choose a PDF to save.")
