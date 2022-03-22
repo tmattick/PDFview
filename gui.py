c280d8f0c79127b3cc4366d1673d7f9f3cc9c869
tmattick <tmattick@smail.uni-koeln.de>
2022-03-22 13:57:21 +0100

    Clean up

--- gui.py
+++ gui.py
@@ -50,6 +50,7 @@ class MainWindow(Window):
         self.selected_pdf: Optional[PDF] = None
         self.event = self.values = None
         self._setup_fig_sub()
+
         self.left_layout = [
             [sg.Text("File:"), sg.In(size=(25, 1), enable_events=True, key="-FILE_IN-", expand_x=True),
              sg.FileBrowse()],
@@ -80,6 +81,7 @@ class MainWindow(Window):
         super().__init__(layout=[[sg.Column(self.left_layout, expand_x=True, expand_y=True), sg.VSeperator(),
                                   sg.Column(self.right_layout, expand_x=True, expand_y=True)]], title="PDFview",
                          finalize=True, resizable=True)
+
         self._draw_figure()
 
     def run(self):
@@ -102,7 +104,10 @@ class MainWindow(Window):
                 self._calc_diff_pdf()
             elif self.event == "-SCALE_BUTTON-":
                 # scale a PDF to a multiple of itself
-                self._scale_pdf()
+                if self.values["-PDF_LIST-"]:
+                    self._scale_pdf()
+                else:
+                    sg.popup_error("Choose a PDF to scale.")
                 self._update_pdf_info()
             elif self.event == "-FIT_BUTTON-":
                 # fit the selected PDF to another one via scaling
@@ -161,12 +166,14 @@ class MainWindow(Window):
     def _scale_pdf(self):
         """Method for scaling :class:`PDF` objects. Draws a new plot with the scaled PDFs on the right-hand canvas.
         """
-        if self.values["-PDF_LIST-"]:
-            pdf_to_scale: PDF = self.values["-PDF_LIST-"][0]
-            pdf_to_scale.scale(float(self.values["-SCALE_IN-"]))
-            self._draw_new_plot()
-        else:
-            sg.popup_error("Choose a PDF to scale.")
+        pdf_to_scale: PDF = self.values["-PDF_LIST-"][0]
+        try:
+            factor = float(self.values["-SCALE_IN-"])
+        except ValueError:
+            sg.popup_error("Your input could not be converted to float.")
+            return
+        pdf_to_scale.scale(factor)
+        self._draw_new_plot()
 
     def _fit_to_pdf(self):
         """Method for scaling :class:`PDF` objects to another :class:`PDF` object. Opens a :class:`FitWindow` object
@@ -200,8 +207,8 @@ class MainWindow(Window):
         plt.close("all")
 
     def _add_to_plot(self):
-        """This method is used for adding a new PDF to existing plots. The new :class:`PDF` has to be the last element
-        in `self.pdfs`. Replaces the old `self.fig_agg`.
+        """This method is used for adding a new PDF to existing plots. The new :class:`PDF` object has to be the last
+        element in `self.pdfs`. Replaces the old `self.fig_agg`.
         """
         self._delete_fig()
         self.sub.plot(self.pdfs[-1].r, self.pdfs[-1].g * self.pdfs[-1].scaling_factor, label=self.pdfs[-1].name)
@@ -212,6 +219,7 @@ class MainWindow(Window):
         Replaces the old `self.fig_agg`."""
         self._delete_fig()
         self._setup_fig_sub()
+
         for p in self.pdfs:
             self.sub.plot(p.r, p.g * p.scaling_factor, label=p.name)
 
@@ -250,9 +258,9 @@ class MainWindow(Window):
         """
         data: List[str] = [pdf.json for pdf in self.pdfs]
         data_json: str = json.dumps(data)
-        data_json_bytes: bytes = zlib.compress(data_json.encode())
+        data_json_compressed: bytes = zlib.compress(data_json.encode())
         with open(path, "wb") as f:
-            f.write(data_json_bytes)
+            f.write(data_json_compressed)
 
     def _load_project(self, path):
         """Loads a project from the provided path. The project data has to be a zlib compressed JSON array containing
@@ -263,8 +271,8 @@ class MainWindow(Window):
         :type path: str
         """
         with open(path, "rb") as f:
-            data_json_bytes: bytes = f.read()
-        data_json: str = zlib.decompress(data_json_bytes).decode()
+            data_json_compressed: bytes = f.read()
+        data_json: str = zlib.decompress(data_json_compressed).decode()
         data: List[str] = json.loads(data_json)
         self.pdfs = [PDF.from_json(json_str) for json_str in data]
         self.window["-PDF_LIST-"].update(self.pdfs)
@@ -280,10 +288,11 @@ class DiffWindow(Window):
     """
 
     def __init__(self, pdfs: List[PDF]):
-        super().__init__([[sg.Listbox(values=pdfs, enable_events=True, size=(20, 5), key="-PDF_MINUENDS-"),
-                           sg.Text(" - "),
-                           sg.Listbox(values=pdfs, enable_events=True, size=(20, 5), key="-PDF_SUBTRAHENDS-")],
-                          [sg.Button("OK", key="-DIFF_BUTTON-")]], "dPDF")
+        layout = [[sg.Listbox(values=pdfs, enable_events=True, size=(20, 5), key="-PDF_MINUENDS-"),
+                   sg.Text(" - "),
+                   sg.Listbox(values=pdfs, enable_events=True, size=(20, 5), key="-PDF_SUBTRAHENDS-")],
+                  [sg.Button("OK", key="-DIFF_BUTTON-")]]
+        super().__init__(layout, "dPDF")
 
     def run(self) -> Optional[PDF]:
         """The event loop for the :class:``DiffWindow``. Returns the dPDF after selecting two :class:`PDF` objects and
@@ -293,7 +302,6 @@ class DiffWindow(Window):
         :rtype: Optional[PDF]
         """
         run_window = True
-        diff_pdf: Optional[PDF] = None
         while run_window:
             event, values = self.window.read()
             if event == "Exit" or event == sg.WIN_CLOSED:
@@ -306,15 +314,13 @@ class DiffWindow(Window):
                 else:
                     sg.popup_error("Please select a minuend and a subtrahend PDF.")
                     continue
-                try:
+                if minuend.x_axes_compatible(subtrahend):
                     diff_pdf: PDF = PDF.differential_pdf(minuend, subtrahend)
-                except XAxisException:
+                    return diff_pdf
+                else:
                     sg.popup_error("The provided PDFs don't share a r axis. dPDF could not be calculated.")
                     continue
-                run_window = False
-                break
         self.window.close()
-        return diff_pdf
 
 
 class FitWindow(Window):
@@ -329,11 +335,12 @@ class FitWindow(Window):
 
     def __init__(self, pdfs: List[PDF], pdf_to_fit: PDF):
         self.pdf_to_fit: PDF = pdf_to_fit
-        super().__init__([[sg.Text("Choose a PDF to scale to.")],
-                          [sg.Listbox(values=pdfs, key="-FIT_TO_PDFS-", size=(20, 5))],
-                          [sg.Text("Fit from"), sg.In(size=(5, 1), key="-FIT_START_IN-"), sg.Text("to"),
-                           sg.In(size=(5, 1), key="-FIT_END_IN-")],
-                          [sg.Button("OK", key="-FIT_BUTTON-")]], "Scale to...")
+        layout = [[sg.Text("Choose a PDF to scale to.")],
+                  [sg.Listbox(values=pdfs, key="-FIT_TO_PDFS-", size=(20, 5))],
+                  [sg.Text("Fit from"), sg.In(size=(5, 1), key="-FIT_START_IN-"), sg.Text("to"),
+                   sg.In(size=(5, 1), key="-FIT_END_IN-")],
+                  [sg.Button("OK", key="-FIT_BUTTON-")]]
+        super().__init__(layout, "Scale to...")
 
     def run(self):
         """Event loop for :class:``FitWindow``. Performs the fitting after the user hits the OK button. Uses
@@ -352,15 +359,12 @@ class FitWindow(Window):
                 else:
                     sg.popup_error("Please select a PDF to fit to.")
                     continue
-
-                if values["-FIT_START_IN-"]:
-                    fit_start: Optional[float] = float(values["-FIT_START_IN-"])
-                else:
-                    fit_start = None
-                if values["-FIT_END_IN-"]:
-                    fit_end: Optional[float] = float(values["-FIT_END_IN-"])
-                else:
-                    fit_end = None
+                try:
+                    fit_start: Optional[float] = float(values["-FIT_START_IN-"]) if values["-FIT_START_IN-"] else None
+                    fit_end: Optional[float] = float(values["-FIT_END_IN-"]) if values["-FIT_END_IN-"] else None
+                except ValueError:
+                    sg.popup_error("Your input could not be converted to float.")
+                    continue
 
                 try:
                     self.pdf_to_fit.scale_to_pdf(fit_pdf, fit_start, fit_end)
